29a396856e855b424c13be1371508d827c2d022b
Aeron Miles <algawolf@protonmail.com>
2021-04-29 14:35:30 +0100

    add mattwaler tea stack template site

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,26 @@
-const fs = require('fs');
-
-module.exports = function (config) {
-  config.setLiquidOptions({
-    dynamicPartials: true,
-  });
-
-  // Static assets to pass through
-  config.addPassthroughCopy('./src/fonts');
-  config.addPassthroughCopy('./src/images');
-  config.addPassthroughCopy('./src/favicon.ico');
-  config.addPassthroughCopy('./src/manifest.json');
-  config.addPassthroughCopy('./src/robots.txt');
-
-  // 404
+module.exports = (config) => {
+  config.addFilter('prettyDate', require('./src/_config/prettyDate'))
+  config.addPassthroughCopy({ public: './' })
+  config.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
   config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404.html');
-
-        browserSync.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
+    files: ['dist/**/*'],
+    open: true,
+    snippetOptions: {
+      rule: {
+        fn: function (snippet, match) {
+          return snippet + match
+        },
+        match: /<\/head>/i,
       },
     },
-  });
-
+  })
+  config.setDataDeepMerge(true)
+  config.setLibrary('md', require('./src/_config/markdown'))
   return {
     dir: {
       input: 'src',
-      output: 'src/_site',
+      output: 'dist',
     },
-    passthroughFileCopy: true,
-    templateFormats: ['html', 'md', 'liquid'],
-    htmlTemplateEngine: 'liquid',
-    dataTemplateEngine: 'liquid',
-    markdownTemplateEngine: 'liquid',
-  };
-};
+    markdownTemplateEngine: false,
+  }
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  plugins: {
-    '@tailwindcss/jit': {},
-    autoprefixer: {},
-  },
-};
+  plugins: [
+    require('postcss-import'),
+    require('@tailwindcss/jit'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production' ? require('cssnano') : null,
+  ],
+}
